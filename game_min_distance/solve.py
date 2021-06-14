62c545b471b0a95874e7e87dc789afb31b39bbc2
cpark7 <94nani0402@gmail.com>
2021-06-14 17:42:25 +0900

    + sovled game_min_distance

--- game_min_distance/solve.py
+++ game_min_distance/solve.py
@@ -25,7 +25,7 @@ def solution(maps):
                     real_map[y+dy[i]][x+dx[i]] =real_map[y][x]+1
                     st.append([y+dy[i],x +dx[i]])
     answer = real_map[n][m]
-
+    print(answer)
     return answer
 
 solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]])
